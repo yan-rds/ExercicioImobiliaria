f8a56e454f594f9788bffc084fc9a634c92062a0
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-09-27 10:56:02 -0300

    Superclasse Imoveis cadastrados criada

---
+++ src/br/com/zup/ImoveisCadastrados.java
@@ -0,0 +1,30 @@
+package br.com.zup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImoveisCadastrados {
+    private List <Imovel> imoveis = new ArrayList<>();
+
+    public ImoveisCadastrados(List<Imovel> imoveis) {
+        this.imoveis = imoveis;
+    }
+
+    public List<Imovel> getImoveis() {
+        return imoveis;
+    }
+
+    public void setImoveis(List<Imovel> imoveis) {
+        this.imoveis = imoveis;
+    }
+
+    public void adicionarImovel (Imovel adicionarImovel){
+        imoveis.add(adicionarImovel);
+    }
+
+    public void mostrarTodosOsImoveis(){
+        for (Imovel referencia : imoveis){
+            referencia.mostrarDados();
+        }
+    }
+}
--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -1,5 +1,7 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
